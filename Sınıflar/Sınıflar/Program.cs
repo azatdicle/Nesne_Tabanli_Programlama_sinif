c8da064f223b058010c3265c1f96da3f45c29653
AZAT DİCLE <75863129+azatdicle@users.noreply.github.com>
2023-11-20 18:45:14 +0300

    a
    a

---
+++ Sınıflar/Sınıflar/Program.cs
@@ -0,0 +1,30 @@
+﻿using Sınıflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+       Ucgen ucgen1 = new Ucgen(); //Ucgen Fonksiyonu oluşturuldu.
+        
+        
+        //Set ornegi
+        /*
+        int acı1;
+        ucgen1.A = -50;
+        */
+
+        //Get örneği
+        /*
+        int acı1;
+        acı1 = ucgen1.A;
+        */
+        Console.ReadKey();
+    }
+}
---
+++ Sınıflar/Sınıflar/Ucgen.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sınıflar
+{
+    public class Ucgen //Heryerden erişilebilir sınıf
+    {
+        int a;
+        int b;
+        int c;
+        
+
+
+        public int kenar1 { get; set; } //Hızlı bir şekilde oluşturma
+
+
+        public int A { //A sınıfı oluşturuldu 
+            // Get ve set veri gönderip almak için kullanılır.
+            get
+            {
+                return A; //Veri göndermek
+            } 
+             set
+            {
+                a= value; //Veri almak
+            }
+        }
+        public int B
+        { //A sınıfı oluşturuldu 
+            // Get ve set veri gönderip almak için kullanılır.
+            get
+            {
+                return B; //Veri göndermek
+            }
+            set
+            {
+                b = value; //Veri almak
+            }
+        }
+        public int C
+        { //A sınıfı oluşturuldu 
+            // Get ve set veri gönderip almak için kullanılır.
+            get
+            {
+                return C; //Veri göndermek
+            }
+            set
+            {
+                c = value; //Veri almak
+            }
+        }
+    }
+}
