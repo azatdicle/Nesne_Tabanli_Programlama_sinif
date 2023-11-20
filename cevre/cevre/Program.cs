f9415e684c3341de13306d7c5ab5dc7b22ab5986
AZAT DİCLE <75863129+azatdicle@users.noreply.github.com>
2023-11-20 18:47:52 +0300

    a
    a

---
+++ cevre/cevre/Program.cs
@@ -0,0 +1,32 @@
+﻿using Sınıflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cevre;
+ internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        sayibulucu nesne = new sayibulucu();
+        //1.Şekil veri girme
+        Console.WriteLine(nesne.BuyukOlaniBul(5, 6));
+        
+        //2 Şekil veri girme
+        //nesne.BuyukOlaniBul(sayi1: 50, sayi2: 50);
+
+        /*
+        Ucgen ucgen1 = new Ucgen();
+        ucgen1.A = 5;
+        ucgen1.B = 5;
+        ucgen1.C = 5;
+
+        Console.WriteLine(ucgen1.Cevre); // Ucgen birin a + b + c değierini gösteriyor
+        */
+
+        Console.ReadKey();
+        }
+}
---
+++ cevre/cevre/sayibulucu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cevre
+{
+    public class sayibulucu
+    {
+                                //Varsayılan olmayan parametreler  en önde olamlı
+        public int BuyukOlaniBul(int c ,int sayi1 = 0,int sayi2=0)
+        {
+            int sonuc;
+            if (sayi1 > sayi2)
+            {
+                sonuc = 1;
+            }
+            else if (sayi1 == sayi2)
+            {
+                sonuc = 0;
+            }
+            else 
+            {
+                sonuc = sayi2;
+            }
+            return sonuc;
+        }
+    }
+}
